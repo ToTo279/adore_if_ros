130cfaa53733a80bff003d6f9d15a7fb13f2a03e
toto279 <tobiasgogo@hotmail.de>
2023-08-16 12:21:55 +0200

    /

--- adore_if_ros/src/adore_unstructured_planner_node.cpp
+++ adore_if_ros/src/adore_unstructured_planner_node.cpp
@@ -14,7 +14,9 @@
 
 #include <adore_if_ros_scheduling/baseapp.h>
 #include <adore_if_ros/factorycollection.h>
-#include <adore/apps/basicunstructuredplanner.h>
+
+#include <adore/apps/trajectory_planner_base.h>
+#include <adore/fun/basicunstructuredplanner.h>
 
 
 namespace adore
@@ -24,7 +26,9 @@ namespace adore
     class UnstructuredPlanNode  : public FactoryCollection, public adore_if_ros_scheduling::Baseapp
     {
       public:
-      adore::apps::BasicUnstructuredPlanner* app_;
+      typedef adore::fun::BasicUnstructuredPlanner TUnstructuredPlanner;
+      TUnstructuredPlanner* app_;
+
       ros::Publisher occupancies_publisher_;
       UnstructuredPlanNode(){}
       void init(int argc, char **argv, double rate, std::string nodename)
@@ -32,26 +36,33 @@ namespace adore
         Baseapp::init(argc, argv, rate, nodename);
         Baseapp::initSim();
         FactoryCollection::init(getRosNodeHandle());
-        app_ = new adore::apps::BasicUnstructuredPlanner();
+
+        app_ = new TUnstructuredPlanner;
+        
         bool use_scheduler = false;
         getRosNodeHandle()->getParam("/use_scheduler",use_scheduler);
+        /*
+        error: no matching function for call to ‘std::function<void()>::function(std::_Bind_helper<false, void (adore::apps::TrajectoryPlannerBase::*)(), adore::fun::BasicUnstructuredPlanner*&>::type)’
         if(use_scheduler)
         {
-          std::function<void()> run_fcn(std::bind(&adore::apps::TrajectoryPlannerBase::planning_request_handler,planner_));
+          //std::function<void()> run_fcn(std::bind(&adore::apps::TrajectoryPlannerBase::planning_request_handler,planner_));
+          std::function<void()> run_fcn(std::bind(&adore::apps::TrajectoryPlannerBase::planning_request_handler,app_));
           Baseapp::addTimerCallback(run_fcn);
         }
         else
         {
           planner_->prime();//node is executed event-based on PlanningRequest, prime sets trigger
-        }
+        }*/
         // timer callbacks
-        std::function<void()> run_fcn(std::bind(&PlotUnstructuredPlannerNode::publish_occupancy_grid,this));
+        std::function<void()> run_fcn(std::bind(&UnstructuredPlanNode::publish_occupancy_grid,this));
         Baseapp::addTimerCallback(run_fcn);
         occupancies_publisher_ = getRosNodeHandle()->advertise<adore_if_ros_msg::PointArray>("occupancies",1);
+        //occupancies_publisher_ = getRosNodeHandle()->advertise<adore_if_ros_msg::PointArrayConstPtr>("occupancies",1);
+        
       }
       void publish_occupancy_grid()
       {
-        adore_if_ros_msg::PointArray msg;
+        adore_if_ros_msg::PointArrayConstPtr msg;
         msg.x = app_->getOccupancies_x();
         msg.y = app_->getOccupancies_y();
         occupancies_publisher_.publish(msg);
